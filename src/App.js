bd2520b0013d9058676184fa39dc6623f65a98fc
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-10-14 11:14:06 +0100

    search added plus data retrieved using fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+
+// components
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
-  return <div className='App'>My App</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
