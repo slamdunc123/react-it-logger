8b311cfd783844c332b0bb060c2a4778e5327486
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-10-15 16:14:10 +0100

    add log working

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -12,7 +12,7 @@ import Preloader from "../layout/Preloader";
 const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
-    // eslint - disable - next - line
+    // eslint-disable-next-line
   }, []);
 
   if (loading || logs === null) {
@@ -34,7 +34,8 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 };
 
 Logs.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
